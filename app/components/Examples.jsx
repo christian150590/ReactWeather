ce7073e0a299b51c423e3c9aeb72e1c6da9d7442
Christian Conde <christian150590@gmail.com>
2017-08-14 18:37:03 -0400

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,11 @@ let React = require('react');
 let Examples = React.createClass({
     render: function () {
         return (
-            <h3>Examples Component !!</h3>
+            <div>
+                <h3>Examples Component</h3>
+                <p>Welcome to example page</p>
+            </div>
+
         )
 
     }
