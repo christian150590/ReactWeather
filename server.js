0c7941d4cc7cf9166cf340b3ec9f5fcef428eef6
Christian Conde <christian150590@gmail.com>
2017-08-15 12:08:28 -0400

    Bug fixed

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'http'){
-      next();
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect(`http://${req.hostname}${req.url}`);
     } else {
-      res.redirect('http://' + req.hostname + req.url);
+        next();
     }
 });
 
